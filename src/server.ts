8f232e85286078d19d2a515cc1cbb6cac7580160
internethero00 <egormix1703@gmail.com>
2025-07-27 20:31:56 +0300

    backup saveDataToFile and restoreDataFromFile

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,30 @@ import {PORT} from "./config/userServerConfig.ts";
 import {userRouters} from "./routers/userRoutes.ts";
 import {UserController} from "./controllers/UserController.ts";
 import {UserServiceEmbeddedImpl} from "./service/UserServiceEmbeddedImpl.ts";
+import {myLogger} from "./utils/logger.ts";
 
-export const launchServer = () => {
+export const launchServer = async () => {
     const userService = new UserServiceEmbeddedImpl();
+    try {
+        await userService.restoreDataFromFile()
+    }catch (error) {}
     const userController:UserController = new UserController(userService);
 
     createServer(async (req, res) => {
       await userRouters(req, res, userController) ;
     }).listen(PORT, () => {
         console.log(`UserServer runs at http://localhost:${PORT}`)
+    })
+
+    process.on('SIGINT',async () => {
+        try {
+            await userService.saveDataToFile()
+        }
+        catch (err){
+
+        }
+        myLogger.log('Saving...')
+        myLogger.saveToFile("Server shutdown by Ctrl+C");
+        process.exit();
     })
 }
--- src/service/UserServiceEmbeddedImpl.ts
+++ src/service/UserServiceEmbeddedImpl.ts
@@ -1,8 +1,13 @@
 import {UserService} from "./UserService.ts";
 import {User} from "../model/userTypes.ts";
+import {UserFilePersistenceService} from "./userFilePersistenceService.ts";
+import fs from "fs";
+import {myLogger} from "../utils/logger.ts";
 
-export  class UserServiceEmbeddedImpl implements UserService{
+export  class UserServiceEmbeddedImpl implements UserService, UserFilePersistenceService{
     private users: User[] = [];
+    private rs = fs.createReadStream('data.txt', {encoding: 'utf8', highWaterMark: 24});
+
     addUser(user: User): boolean {
         if(this.users.findIndex((u:User) => u.id === user.id) === -1)
         {
@@ -34,5 +39,58 @@ export  class UserServiceEmbeddedImpl implements UserService{
         const index = this.users.findIndex(item => item.id === newUser.id);
         if(index === -1) throw "404";
         this.users[index] = newUser;
+    }
+
+    async restoreDataFromFile(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            let result: string = "";
+            this.rs.on('data', (chunk) => {
+                if(chunk) {
+                    result += chunk.toString();
+                }
+                else {
+                    result = "[]";
+                }
+            })
+            this.rs.on('end', () => {
+                if(result) {
+                    this.users = JSON.parse(result);
+                    myLogger.log("Data was restored from file");
+                    myLogger.save("Data was restored from file");
+                    this.rs.close();
+                    resolve();
+                }
+                else {
+                    this.users = [{id: 123, userName: 'Grushin'}]
+                    resolve();
+                }
+            })
+
+            this.rs.on('error', () => {
+                this.users = [{id: 2, userName: 'Bender'}]
+                myLogger.log("File to restore not found");
+                reject()
+            })
+        })
+
+    }
+
+    async saveDataToFile(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const ws = fs.createWriteStream('data.txt')
+            const data = JSON.stringify(this.users);
+            ws.write(data);
+            ws.on('end', () => {
+                myLogger.log("Data was saved to file");
+                myLogger.save("Data was saved to file");
+                ws.close();
+                resolve();
+            })
+            ws.on('error', () => {
+                myLogger.log("error: data not saved")
+                reject()
+            })
+        })
+
     }
 }
